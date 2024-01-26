fb0449889a4311471cf55d693e64b915db68444e
Liubomyr Yarych <78175599+yarychok@users.noreply.github.com>
2024-01-26 11:20:37 +0200

    Added and configured Navigation bar Items component.

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,9 +1,32 @@
 "use client"
 
+import { PRODUCT_CATEGORIES } from "@/config"
+import { useState } from "react"
+import NavItem from "./NavItem"
+
 const NavItems = () => {
+
+    const [activeIndex, setActiveIndex] = useState<null | number>(null)
+
+    const isAnyOpen = activeIndex !== null
+
     return (
         <div className="flex gap-4 h-full">
-            
+            {PRODUCT_CATEGORIES.map((category, i) => {
+
+                const handleOpen = () => {
+                    if(activeIndex === i) {
+                        setActiveIndex(null)
+                    } else {
+                        setActiveIndex(i)
+                    }
+                }
+
+                const isOpen = i === activeIndex
+
+                return <NavItem category={category} handleOpen={handleOpen} isOpen={isOpen} isAnyOpen={isAnyOpen} key={category.value} />
+                
+            })}
         </div>
     )
 }
--- src/config/index.ts
+++ src/config/index.ts
@@ -7,7 +7,38 @@ export const PRODUCT_CATEGORIES = [
                 name: "Editor picks",
                 href: "#",
                 imageSrc: "/nav/ui-kits/mixed.jpg",
-            }
+            },
+            {
+                name: "New Arrivals",
+                href: "#",
+                imageSrc: "/nav/ui-kits/blue.jpg",
+            },
+            {
+                name: "Bestsellers",
+                href: "#",
+                imageSrc: "/nav/ui-kits/purple.jpg",
+            },
+        ]
+    },
+    {
+        label: "Icons",
+        value: "icons" as const,
+        featured: [
+            {
+                name: "Favorite Icon Picks",
+                href: "#",
+                imageSrc: "/nav/icons/picks.jpg",
+            },
+            {
+                name: "New Arrivals",
+                href: "#",
+                imageSrc: "/nav/icons/new.jpg",
+            },
+            {
+                name: "Bestselling Icons",
+                href: "#",
+                imageSrc: "/nav/icons/bestsellers.jpg",
+            },
         ]
     }
 ]
