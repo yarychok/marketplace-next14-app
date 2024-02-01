98a73d5996301709ba3fab4a2ba993ffb18213cc
Liubomyr Yarych <78175599+yarychok@users.noreply.github.com>
2024-02-01 12:28:24 +0200

    Setting up the express server & added client caching.

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from "express"
+import { getPayloadClient } from "./get-payload"
+
+const app = express()
+const PORT = Number(process.env.PORT) || 3001
+
+const start = async () => {
+    const payload = await getPayloadClient({
+        initOptions: {
+            express: app,
+            onInit: async (cms) => {
+                cms.logger.info(`Admin URL ${cms.getAdminURL()}`)
+            }
+        }
+    })
+}
+
+start()
